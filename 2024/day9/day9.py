13dacdd1db831111e48efd6b5aaae2aa6f81eca3
veselin-antonov <veselin.antonov.01@gmail.com>
2024-12-12 13:14:53 +0200

    Second variant for part 2 and some fixes

--- 2024/day9/day9.py
+++ 2024/day9/day9.py
@@ -1,6 +1,6 @@
 # Day 9: Disk Fragmenter
 
-import sys, re
+import sys
 
 sys.setrecursionlimit(10**5)
 
@@ -11,25 +11,25 @@ def parseData(compressedData):
         dataBlockSize = int(compressedData[i])
         
         for _ in range (dataBlockSize):
-            data.append(str(dataID))
+            data.append(dataID)
             
         emptyBlockSize = int(compressedData[i + 1])
         
         for _ in range (emptyBlockSize):
-            data.append('.')
+            data.append(-1)
             
         dataID += 1
             
     return data
 
-def parseFileDataRecursively(compressedData, dataID = 0):
-    if len(compressedData) == 0:
+def parseDataRecursively(data, dataID = 0):
+    if len(data) == 0:
         return []
     else:
-        data = list([str(dataID) for _ in range(int(compressedData[0]))])
-        data.extend(list(['.' for _ in range(int(compressedData[1]))]))
-        data.extend(parseFileDataRecursively(compressedData[2:], dataID + 1))
-        return data
+        parsedData = list([dataID for _ in range(int(data[0]))])
+        parsedData.extend(list([-1 for _ in range(int(data[1]))]))
+        parsedData.extend(parseDataRecursively(data[2:], dataID + 1))
+        return parsedData
     
 def parseDataInGroups(compressedData):
     data = []
@@ -49,8 +49,8 @@ def parseDataInGroups(compressedData):
 def compressData(dataBlocks):
     index = 0
     while index < len(dataBlocks):
-        if dataBlocks[index] == '.':
-            while dataBlocks[len(dataBlocks) - 1] == '.':
+        if dataBlocks[index] == -1:
+            while dataBlocks[len(dataBlocks) - 1] == -1:
                 dataBlocks.pop()
                 
             if (index >= len(dataBlocks)):
@@ -64,11 +64,11 @@ def compressData(dataBlocks):
 def compressDataRecursively(dataBlocks):
     if len(dataBlocks) == 0:
         return []
-    elif dataBlocks[0] != '.':
+    elif dataBlocks[0] != -1:
         data = [dataBlocks[0]]
         data.extend(compressDataRecursively(dataBlocks[1:]))
         return data
-    elif dataBlocks[-1] == '.':
+    elif dataBlocks[-1] == -1:
         return compressDataRecursively(dataBlocks[:-1])
     else:
         data = [dataBlocks.pop()]
@@ -97,15 +97,64 @@ def compressDataWithoutFragmentation(dataBlockGroups: list):
 def checkSum(data):
     return sum([data[i] * i for i in range(len(data)) if data[i] > 0])
     
-def part1(inputFile):
+def variant1(inputFile):
     with open(inputFile, 'r') as file:
             data = file.read().strip() + '0'
             
-    parsedData = parseDataInGroups(data)
+    parsedData = parseData(data)
+    # parsedData = parseDataRecursively(data)
+    parsedData2 = parseDataInGroups(data)
             
-    compressedData = compressDataWithoutFragmentation(parsedData)
+    compressedData = compressData(parsedData)
+    # compressedData = compressDataRecursively(parsedData)
+    compressedData2 = compressDataWithoutFragmentation(parsedData2)
     
-    print(compressedData)
-    print(checkSum(compressedData))
+    print('Part 1: ', checkSum(compressedData))
+    print('Part 2: ', checkSum(compressedData2))
     
-part1('day9-input.txt')
+variant1('day9-input.txt')
+
+class Memory():
+    def __init__(b, pos, size):
+        b.pos = pos  # Starting position of the block
+        b.size = size  # Length of the block
+        
+    def __repr__(b):
+        return f'Mem(pos: {b.pos}, size: {b.size})'
+    
+    # Calculate the value of the block based on the indexes it takes up using teh formula for Arithmetic Progression
+    def value(b):
+        return (2 * b.pos + b.size - 1) * b.size // 2
+    
+def parseMemoryBlocks(diskData):
+    memoryBlocks = []
+    position = 0
+    for bitSize in map(int, diskData):
+        memoryBlocks.append(Memory(position, bitSize))
+        position += bitSize
+        
+    return memoryBlocks
+
+def compressMemory(memoryBLocks):
+    for fileBlock in memoryBLocks[::-2]:
+        for emptyBlock in memoryBLocks[1::2]:
+            if (emptyBlock.pos < fileBlock.pos
+            and emptyBlock.size >= fileBlock.size):
+                fileBlock.pos = emptyBlock.pos
+                emptyBlock.pos += fileBlock.size
+                emptyBlock.size -= fileBlock.size
+                
+def checkSumMemory(memoryBlocks):
+    return sum(id * memoryBlock.value() for id, memoryBlock in enumerate(memoryBlocks[::2]))
+        
+def variant2(inputFile):
+    with open(inputFile, 'r') as file:
+        diskData = file.read().strip()
+        
+    memoryBlocks = parseMemoryBlocks(diskData)
+    
+    compressMemory(memoryBlocks)
+    
+    print('Part 2 (V2): ', checkSumMemory(memoryBlocks))
+    
+variant2('day9-input.txt')
