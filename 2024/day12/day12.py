89e93a0ec7fffd61b5b9d8f085cb276e7a4cd98d
veselin-antonov <veselin.antonov.01@gmail.com>
2024-12-14 17:15:33 +0200

    WIP

--- 2024/day12/day12.py
+++ 2024/day12/day12.py
@@ -25,15 +25,32 @@ def isInsideMap(pos, rows, cols):
     row, col = pos
     return not (row < 0 or row >= rows or col < 0 or col >= cols)
 
-def exploreRegion(symbol, pos, direction, garden):
+def exploreRegion(symbol, pos, direction, garden, visited, edges):
     if not isInsideMap(pos, len(garden), len(garden[0])) or symbol != garden[pos[0]][pos[1]]:
-        return []
-    
-    newPos = direction.move(pos)
-    return [pos] + exploreRegion(symbol, newPos, direction, garden) + exploreRegion(symbol, newPos, direction.turnLeft(), garden) + exploreRegion(symbol, newPos, direction.turnRight(), garden)
+        edges.append(pos)
+        return
+    elif pos in visited:
+        return
+    else:    
+        visited.add(pos)
+        exploreRegion(symbol, direction.move(pos), direction, garden, visited, edges)
+        exploreRegion(symbol, direction.turnLeft().move(pos), direction.turnLeft(), garden, visited, edges)
+        exploreRegion(symbol, direction.turnRight().move(pos), direction.turnRight(), garden, visited, edges)
     
 
 with open('day12-input.txt', 'r') as file:
-    garden = file.read()
+    garden = file.read().split('\n')
     
-    print(exploreRegion('C', (0, 0), Direction.EAST, garden))
+    result = 0
+    regions = []
+    for row in range(len(garden)):
+        for col in range(len(garden[0])):
+            if all(not (row, col) in region[0] for region in regions):
+                region = set()
+                edges = [(row - 1, col)]
+                exploreRegion(garden[row][col], (row, col), Direction.EAST, garden, region, edges)
+                regions.append((region, edges))
+                print(len(region), end = " ")
+                print(len(edges) + 1)
+                
+print(sum([len(region[0]) * len(region[1]) for region in regions]))
